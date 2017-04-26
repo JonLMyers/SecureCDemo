8110d4e22d1d1ba1ac74275f596fd911cd7d2448
Jon Myers <jlm4508@rit.edu>
2017-04-26 12:07:27 -0400

    Base

---
+++ input.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int getUserInput()
+{
+
+
+    return 0;
+}
+
+int main()
+{
+    printf("########################################\n");
+    printf("Welcome to the Stratux Payment Processor\n");
+    printf("########################################\n\n");
+
+    return 0;
+}
